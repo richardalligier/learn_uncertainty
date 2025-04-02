de6d890c6e0718aa6a18241e99c2d8a020d66139
Richard ALLIGIER <24826685+richardalligier@users.noreply.github.com>
2025-04-02 11:19:59 +0200

    a

--- fit_traj.py
+++ fit_traj.py
@@ -226,7 +226,7 @@ def compute_t_rejoin(sit,thresh,nwpts):
     # print(l)
     # print(whichbeacon,selectednbok)
     # raise Exception
- 
+
 # new axis SITUATION et axe OTHERS pour les autres avions
 def convert_situation_to_flights(sit,initialize,device,thresh_xy):
     df = sit.trajectories.copy()
@@ -526,254 +526,48 @@ def test_uncertainty(sit,fdeviated,fothers,device):
 # def test_error(sit):
 
 # raise Exception
-def main():
 
+def test_animate():
     import torchtraj
-
     import torch
     import matplotlib.pyplot as plt
     from traffic.data import opensky
     torch.random.manual_seed(0)
     fname = "data/AA38909998_1657916586_1657917209.json"
-    fname = "data/AA38932291_1657920628_1657920857.json"
+    #fname = "data/AA38932291_1657920628_1657920857.json"
     #fname = "data/AA38944134_1658001122_1658001604.json"
     sit = read_json.Situation.from_json(fname)#.cut()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     fdeviated,fothers = convert_situation_to_flights(sit,initialize,device,thresh_xy=THRESH_XY_MODEL)
     torch.save(fothers.serialize(),fname[:-4]+"pt")
-    fothers = deserialize(torch.load(fname[:-4]+"pt")).to(device)
+    fothersc = deserialize(torch.load(fname[:-4]+"pt")).to(device)
+    for k in fothers.dictparams():
+        print(k)
+        if k in ["fxy","fz"]:
+            for k2 in getattr(fothersc,k).dictparams():
+                print(k2)
+                print((getattr(getattr(fothersc,k),k2) == getattr(getattr(fothers,k),k2)).all())
+        else:
+            print((getattr(fothersc,k) == getattr(fothers,k)).all())
     # raise Exception
-    fdeviated = fdeviated.clone()
+    # raise Exception
+    # fdeviated = fdeviated.clone()
     test_uncertainty(sit,fdeviated,fothers,device)
-    raise Exception
-    f = fdeviated.fxy[0]
-    t = torch.arange(0,f.duration.sum(axis=-1).max()+100,device=device).rename(T)
-    xory=False
-    plot(f,t,xory)
-    if not xory:
-        plt.gca().axis('equal')
-    deviated = sit.trajectories.query("flight_id==@sit.deviated.flight_id").query("timestamp==@sit.deviated.stop")
-    # plt.scatter(beacon.x,beacon.y,marker="*")
-    plt.scatter(deviated.x, deviated.y)
-    t_pts = torch.tensor([fdeviated.tdeviation,fdeviated.tturn,fdeviated.trejoin],dtype=torch.float,names=(WPTS,),device=device)
-    #t_pts = torch.tensor([fdeviated.tturn,fdeviated.tturn,fdeviated.tturn],dtype=torch.float,names=(WPTS,),device=device)
-    xy_pts = traj.generate(fdeviated.fxy[0],t_pts.rename(**{WPTS:T})).cpu()
-    recplot(xy_pts,plt.scatter)
-    # fnew = fdeviated.fxy[0].add_wpt_at_t(t_pts)
-    # plot(fnew,t,xory)
-    # raise Exception
-    # print(fdeviated.fxy[0].wpts_at_t(t_pts))
-    # raise Exception
-    # t = torch.arange(fdeviated.fxy[0].duration.sum(),device=device).rename(T)
-    # xy_pts = traj.generate(fdeviated.fxy[0],t).cpu()
-    # # recplot(xy_pts,plt.scatter)
-    # xy_pts2 = traj.generate(fnew,t).cpu()
-    # recplot(xy_pts,plt.scatter)
-    # print(xy_pts)
-    # raise Exception
-    # for beacon,trajp in lwpts:
-    #     plt.scatter(beacon.x,beacon.y,marker="*")
-    #     plt.scatter(trajp.x,trajp.y)
-    plt.show()
-    raise Exception
-    # print(sit.trajectories.query("timestamp==@sit.deviated.start"))
-    print(sit.deviated.flight_id)
-    selected_fid = sit.deviated.flight_id
-    # selected_fid = np.int64(38910912)
-    print(sit.trajectories.groupby("flight_id").count())
-    # raise Exception
-    #selected_fid = np.int64(38926951)
-    dfin = sit.trajectories.query("flight_id==@selected_fid")#.loc[1:]
-    lres = []
-    # for f in Traffic(dfin):
-    #     print(f.aligned_on_navpoint(sit.deviated.beacons))pd.concat(lres,ignore_index=True)
-    # df = Traffic(dfin).simplify(100.).data
 
-
-    # da = dfin.copy()
-    # da["track"]=da["track_angle"]
-    # da["timestamp"]=pd.to_datetime(da["timestamp"],unit='s')
-    # # print(df.dtypes)
-    # for flight in Traffic(da):
-    #     print(flight.data)
-    #     print(list(flight.data))
-    #     for x in flight.aligned_on_navpoint(points=sit.deviated.beacons):
-    #         print(x)
-    # raise Exception
-    # print(df)
-    # raise Exception
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    sitflights = convert_situation_to_flights(sit,initialize,device)#.fxy[2]
-    quality_check(sit, sitflights)
-
-    # if not xory:
-    #     plt.gca().axis('equal')
-    # plot(sitflights.fxy[0],sitflights.t[0],xory)
-    # xydev = traj.generate(sitflights.fxy[0],torch.tensor([sitflights.tdeviation],device=device,names=(T,))).cpu()
-    # plt.scatter(xydev[...,0],xydev[...,1])
-    # real = sit.trajectories.query("flight_id==@sit.deviated.flight_id").query("timestamp==@sit.deviated.start")
-    # plt.scatter(real.x,real.y)
-    # plt.show()
-    # raise Exception
-
-    # df = sit.trajectories#.query("flight_id == @selected_fid")
-    # print(df.flight_id)
-    # # raise Exception
-    # df["timestamp"] = pd.to_datetime(df["timestamp"],unit="s")
-    # # print(df)
-    # # print(df.dtypes)
-    # # raise Exception
-    # icao24 = df.icao24.values[0]
-    # print(icao24)
-    # # plt.show()
-    # # df = opensky.history(start=unix2datetime(df.timestamp.values[0]),#.strftime('%Y-%m-%d %H:%M:%S')
-    # #                      stop=unix2datetime(df.timestamp.values[-1]),#.strftime('%Y-%m-%d %H:%M:%S'),
-    # #                      icao24=icao24)
-    # # df = df.compute_xy(projection=read_json.PROJ).data
-    # plt.plot(df.x,df.y)
-    # print(unix2datetime(sit.deviated.start))
-    # # plt.plot(df.timestamp,df.track)
-    # # plt.show()
-    # # plt.scatter(*read_json.PROJ.transform([p.longitude for p in sit.deviated.beacons],[p.latitude for p in sit.deviated.beacons]),marker="*",color="red")
-
-    # dbeacons={}
-    # for flight in Traffic(df):
-    #     for beacon,color in zip(sit.deviated.beacons,["red","blue","green","brown","black","yellow"]*4):
-    #         lbeacon = [beacon]
-    #         for x in flight.aligned_on_navpoint(points=lbeacon,**KIM_PARAMETERS):
-    #             for _,line in x.data.iterrows():
-    #                 dbeacons[line.timestamp]=dbeacons.get(line.timestamp,[])+[beacon]
-    #             plt.scatter(*read_json.PROJ.transform([p.longitude for p in lbeacon],[p.latitude for p in lbeacon]),marker="*",color=color)
-    #             plt.scatter(x.data.x,x.data.y,color=color)
-    # for k,v in dbeacons.items():
-    #     print(k,len(v))
-    # # print(dbeacons)
-    # # print(df.query("timestamp==@datetime.datetime.fromtimestamp(@sit.deviated.start,datetime.UTC)")[["timestamp","longitude","latitude"]])
-    # # raise Exception
-
-    # #df = sit.trajectories.query("flight_id == @sitflights.fid[0]")
-    # #plt.scatter(df.x,df.y)
-
-    # plt.show()
-    # raise Exception
-
-    fmodel = sitflights.fxy[0]
-    DANGLE = "dangle"
-    MAN_WPTS = "man_wpts"
-    DT = "dt1"
-    DSPEED = "dspeed"
-    #wpts_start = 3+torch.tensor([0],device=device,dtype=torch.int64).reshape(1,-1).rename(BATCH,MAN_WPTS)#.reshape(-1).rename(BATCH)#,DANGLE)
-    wpts_start = 6+torch.tensor([0],device=device,dtype=torch.int64).reshape(-1).rename(BATCH)#.reshape(-1).rename(BATCH)#,DANGLE)
-    # print(f"{wpts_start=}")
-    wpts_turn = wpts_start+2
-    wpts_rejoin = wpts_turn+2
-    dangle = 1 + torch.arange(1,device=device).reshape(-1).rename(DANGLE)
-    dt = 300+20*torch.arange(1,device=device).reshape(-1).rename(DT)
-    dspeed = 1.5+torch.arange(1,device=device).reshape(-1).rename(DSPEED)
-    # dangle[0]=1#-0.3
-    # print(dangle)
-    #dt = 40+torch.arange(1,device=device,dtype=torch.int64).reshape(1,1).rename(BATCH,DANGLE)#torch.arangeones_like(wpts_turn)*10#np.pi/2
-    # print(wpts_start,wpts_turn,wpts_rejoin)
-    # raise Exception
-    #fs = uncertainty.addangle(dangle,wpts_start,wpts_turn,wpts_rejoin,fmodel)
-    fs = uncertainty.addangle(dangle,wpts_start,wpts_turn,wpts_rejoin,fmodel)
-    # fs = uncertainty.adddt_old(dt,wpts_start[0].item(),(wpts_start[0].item(),wpts_turn[0].item()),fmodel)
-    #fs = uncertainty.adddt_translate(dt,wpts_start,wpts_turn,fmodel)
-    #fs = uncertainty.adddt_rotate(dt,wpts_start,wpts_turn,fmodel)
-    # fs = uncertainty._contract(fold,fmodel,wpts_start,wpts_turn)
-    #fs = uncertainty.adddt_rotate(dt,wpts_turn,wpts_rejoin,fmodel)
-    # fs = uncertainty.contract(fs,fmodel,wpts_turn,wpts_rejoin,ratio)
-    #fs = uncertainty.changespeed(dspeed,wpts_start,wpts_turn,wpts_rejoin,fmodel)
-    #fc = uncertainty.addangle_old(dangle,wpts_start[0,0].item(),(wpts_start[0,0].item(),wpts_turn[0,0].item()),fmodel)
-    # fs = uncertainty.adddt(dangle,wpts_start,wpts_turn,wpts_rejoin,fmodel)
-    # fc = uncertainty.adddt_old(dangle,wpts_start[0].item(),(wpts_start[0].item(),wpts_turn[0].item()),fmodel)
-    # fs = initialize(df,t_zero,device)
-    #t = t_from_df(dfin,device)
-    t = torch.arange(0.,fmodel.duration.sum(axis=-1).max()+400,device=device).rename(T)
-    #torch.tensor(dfin.timestamp.values-dfin.timestamp.values[0],device=device,dtype=torch.float32).rename(T)
-    trajreal = trajreal_from_df(dfin,device)
-    print(fmodel.duration)
-    #f = fit.fit(fmodel,trajreal,t,traj.generate,10000)
-    f = fmodel
-    print(f.meanv())
-    print(f.duration)
-    print(fs.meanv())
-
-
-    # f = fit.fit(fmodel,trajreal,t,traj.generate,10000)
-    # print(f)
-    # print(fc)
-    # print(fs)
-    # trajreal = trajreal.cpu()
-    # plt.scatter(dfin.x,dfin.y)
-    # xy  = traj.generate(f,t).cpu()
-    # plt.scatter(xy[0,:,0],xy[0,:,1])
-
-    plot(f,t,xory)
-    plot(fs,t,xory)
-    if not xory:
-        plt.gca().axis('equal')
-    # plot_xy(xy,wpts)
-    # def printxy(xy,wpts):
-    # n_dim_param = xy.ndim() - 2
-    # for ibatch  in range(xy.shape[0]):
-    #     for iangle  in range(xy.shape[1]):
-    #         xyc  = xy[ibatch,iangle]
-    #         wptsc  = wpts[ibatch,iangle]
-    #         plt.scatter(xyc[:,0],xyc[:,1])
-    #         plt.scatter(wptsc[:,0],wptsc[:,1])
-    # plt.scatter(df.x,df.y)
-    # plt.gca().axis('equal')
-    # print(f)
-    # print(f.compute_wpts().cpu())
-    plt.show()
-
-    raise Exception
-    print(t.shape)
-    print(xy[0,:,1].shape)
-    # plt.scatter(torch.arange(0,t.max()),xy[0,:,1])
-    # xy = traj.generate(f,t)
-    print(fmodel.meanv())
-    print(fs.meanv())
-    errx=dfin.x.values-xy[0,:,0].cpu().numpy()
-    erry=dfin.y.values-xy[0,:,1].cpu().numpy()
-    plt.scatter(t.cpu(),np.sqrt(errx**2+erry**2))
-    xy  = traj.generate(fs,t).cpu()
-    errx=dfin.x.values-xy[0,:,0].cpu().numpy()
-    erry=dfin.y.values-xy[0,:,1].cpu().numpy()
-    plt.scatter(t.cpu(),np.sqrt(errx**2+erry**2))
-    plt.show()
-
-    t = torch.arange(0,t.max(),device=device,dtype=torch.float32).rename(T)
-    xy =  traj.generate(f,t).cpu()
-    dist = xy[0,:,0]#np.sqrt(np.diff(xy[0,:,0]) ** 2 + np.diff(xy[0,:,1]) ** 2)
-    plt.plot(dist)
-    # plt.plot(dist/np.diff(t.cpu()))
-    xy  = traj.generate(fs,t).cpu()
-    dist = xy[0,:,0]#np.sqrt(np.diff(xy[0,:,0]) ** 2 + np.diff(xy[0,:,1]) ** 2)
-    plt.plot(dist)
-    # dist = np.sqrt(np.diff(dfin.x.values) ** 2 + np.diff(dfin.y.values) ** 2)
-    # plt.plot(dist/np.diff(dfin.timestamp.values))
-    plt.show()
-    t = torch.arange(0,t.max(),device=device,dtype=torch.float32).rename(T)
-    xy =  traj.generate(f,t).cpu()
-    dist = np.sqrt(np.diff(xy[0,:,0]) ** 2 + np.diff(xy[0,:,1]) ** 2)
-    plt.plot(dist/np.diff(t.cpu()))
-    xy  = traj.generate(fs,t).cpu()
-    dist = np.sqrt(np.diff(xy[0,:,0]) ** 2 + np.diff(xy[0,:,1]) ** 2)
-    plt.plot(dist/np.diff(t.cpu()))
-    plt.show()
-
-
-    # fopti = fit.fit(fmodel,trajreal,t,traj.generate,10000)
-    # xy  = traj.generate(fopti,t)
-    # print(xy,xy.shape)
-    # print(trajreal)
-    # print(fopti)
-    # for fid in sit.trajectories.flight_id.unique():
-    #     traj = sit.trajectories.query("flight_id==@fid").query("timestamp==@sit.deviated.start")
-    #     plt.scatter(traj.longitude,traj.latitude)
-    # plt.show()
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='fit trajectories and save them in folders',
+    )
+    parser.add_argument('-json')
+    parser.add_argument('-deviated')
+    parser.add_argument('-others')
+    args = parser.parse_args()
+    sit = read_json.Situation.from_json(args.json)
+    device = "cpu"
+    fdeviated,fothers = convert_situation_to_flights(sit,initialize,device,thresh_xy=THRESH_XY_MODEL)
+    torch.save(fdeviated.serialize(),args.deviated)
+    torch.save(fothers.serialize(),args.others)
 
 if __name__ == "__main__":
     main()
